5a2729e84e26eb778b723a1d9c501f38a216b162
Inna Kotkovets <158862790+Inna-Kot@users.noreply.github.com>
2024-11-08 22:01:35 +0000

    Start with first welcome function and write text for the beginning of the game

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome():
+    print("""The shipwreck was sudden and violent. You woke up on the shore of a deserted island, surrounded by the wreckage. 
+As you stood, aching and disoriented, you heard angry shouts. A group of indigenous people emerged from the jungle,
+running towards you with alarming speed. You had to decide quickly: [run] into the forest to hide or [dive] back 
+to the ship's remains on the beach to find a weapon. Time was running out, and your fate depended on your next move.""")
