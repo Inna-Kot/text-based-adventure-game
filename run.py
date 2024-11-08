fa0ad1c8bdf89c2437e2004688f24dccad99fc2b
Inna Kotkovets <158862790+Inna-Kot@users.noreply.github.com>
2024-11-08 22:14:45 +0000

    add input and create if statement with new functions

--- run.py
+++ run.py
@@ -1,5 +1,19 @@
 def welcome():
-    print("""The shipwreck was sudden and violent. You woke up on the shore of a deserted island, surrounded by the wreckage. 
-As you stood, aching and disoriented, you heard angry shouts. A group of indigenous people emerged from the jungle,
-running towards you with alarming speed. You had to decide quickly: [run] into the forest to hide or [dive] back 
-to the ship's remains on the beach to find a weapon. Time was running out, and your fate depended on your next move.""")
+    print("""The shipwreck was sudden and violent. You woke up on the shore
+of a deserted island, surrounded by the wreckage. As you stood, aching and
+disoriented, you heard angry shouts. A group of indigenous people emerged from
+the jungle, running towards you with alarming speed. You had to decide quickly:
+[run] into the forest to hide or [dive] back to the ship's remains on the
+beach to find a weapon. Time was running out, and your fate depended
+on your next move.""")
+    option = input("")
+    if option == 'dive':
+        remains()
+    elif option == 'run':
+        forest()
+    else:
+        invalidoption()
+        welcome()
+
+
+welcome()
